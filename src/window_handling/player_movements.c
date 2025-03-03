b848d7bb430745ac244030b611931ce9dd311b74
Oceane TOROSSIAN <octoross@paul-f4Ar6s9.clusters.42paris.fr>
2025-03-03 19:11:03 +0100

    Clean(srcs): norminette

--- src/window_handling/player_movements.c
+++ src/window_handling/player_movements.c
@@ -3,14 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   player_movements.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jermarti <jermarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 03:08:27 by jermarti          #+#    #+#             */
-/*   Updated: 2025/03/01 03:34:52 by jermarti         ###   ########.fr       */
+/*   Updated: 2025/03/03 19:09:19 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+int	new_player_coords(float *x, float *y, t_player *player, int key)
+{
+	if (((key == QWERTY_RIGHT) && QWERTY_MODE)
+		|| ((key == AZERTY_RIGHT) && !QWERTY_MODE))
+		return (*x = -player->dy * SPEED,
+			*y = player->dx * SPEED, 1);
+	else if (((key == QWERTY_LEFT) && QWERTY_MODE)
+		|| ((key == AZERTY_LEFT) && !QWERTY_MODE))
+		return (*x = player->dy * SPEED,
+			*y = -player->dx * SPEED, 1);
+	else if (((key == QWERTY_FORWARD) && QWERTY_MODE)
+		|| ((key == AZERTY_FORWARD) && !QWERTY_MODE))
+		return (*x = player->dx * SPEED,
+			*y = player->dy * SPEED, 1);
+	else if (key == BACKWARD)
+		return (*x = -player->dx * SPEED,
+			*y = -player->dy * SPEED, 1);
+	return (0);
+}
 
 void	move_player(t_map *game, t_player *player, int key)
 {
@@ -19,27 +39,7 @@ void	move_player(t_map *game, t_player *player, int key)
 
 	move_x = 0;
 	move_y = 0;
-	if (((key == QWERTY_RIGHT) && QWERTY_MODE) || ((key == AZERTY_RIGHT) && !QWERTY_MODE))
-	{
-		move_x = -player->dy * SPEED;
-		move_y = player->dx * SPEED;
-	}
-	else if (((key == QWERTY_LEFT) && QWERTY_MODE) || ((key == AZERTY_LEFT) && !QWERTY_MODE))
-	{
-		move_x = player->dy * SPEED;
-		move_y = -player->dx * SPEED;
-	}
-	else if (((key == QWERTY_FORWARD) && QWERTY_MODE) || ((key == AZERTY_FORWARD) && !QWERTY_MODE))
-	{
-		move_x = player->dx * SPEED;
-		move_y = player->dy * SPEED;
-	}
-	else if (key == BACKWARD)
-	{
-		move_x = -player->dx * SPEED;
-		move_y = -player->dy * SPEED;
-	}
-	else
+	if (!new_player_coords(&move_x, &move_y, player, key))
 		return ;
 	if ((game->map[(int)player->y][(int)(player->x + move_x)] != '1')
 		&& (game->map[(int)(player->y + move_y)][(int)player->x] != '1'))
